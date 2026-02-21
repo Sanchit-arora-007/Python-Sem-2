6da64a0e6c9f57af1aa90612d556c3f15f4cac16
Sanchit Arora <arorasanchit07@gmail.com>
2026-02-21 12:49:56 +0530

    Python Codes-Sem-2

---
+++ Python Lab/Experiment4/q8.py
@@ -0,0 +1,7 @@
+S1 = {"Red", "yellow", "orange", "blue"}
+S2 = {"violet", "blue", "purple"}
+print("Union:", S1.union(S2))
+print("Intersection:", S1.intersection(S2))
+print("S1 - S2:", S1.difference(S2))
+print("S2 - S1:", S2.difference(S1))
+print("Symmetric Difference:", S1.symmetric_difference(S2))
