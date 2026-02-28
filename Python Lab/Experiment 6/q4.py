8396dd204dfc4cdc86e75fbc145d09de50c14a57
Sanchit Arora <arorasanchit07@gmail.com>
2026-02-28 10:31:49 +0530

    Python Codes-Sem-2

---
+++ Python Lab/Experiment 6/q4.py
@@ -0,0 +1,10 @@
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+
+def print_fibonacci(n):
+    for i in range(n):
+        print(fibonacci(i), end=" ")
+
+print_fibonacci(7)
